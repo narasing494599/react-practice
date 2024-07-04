ff99eebb2d5d3aa6d76e1cd680558ae531a4616a
Narasing Kokane <narasingkokane@INBLR1MACFW74TM.local>
2024-07-04 09:40:20 +0530

    loading content from data js file

---
+++ react-state/src/App.jsx
@@ -0,0 +1,51 @@
+import { CORE_CONCEPTS } from '../src/assets/data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
+import {EXAMPLES} from '../src/assets/data.js'
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+  function handleSelect(selectedTab) {
+    setSelectedTopic(selectedTab);
+}
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          <div id='dynamic-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+          <h3></h3>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
